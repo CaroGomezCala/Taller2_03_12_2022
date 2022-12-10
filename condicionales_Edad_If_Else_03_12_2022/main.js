0d01b1a75c3b1cc61a7b739b5cc2abcb2fc0d94b
CaroGomezCala <lcgk.021@gmail.com>
2022-12-10 11:10:08 -0500

    Solucion_1

---
+++ condicionales_Edad_If_Else_03_12_2022/main.js
@@ -0,0 +1,5 @@
+addEventListener("DOMContentLoaded", (e)=>{
+    let edad = Number(prompt("Digita por favor tu Edad: "));
+    if(edad>=18) console.log("Es mayor de edad");
+    else console.log("La edad ingresada no cumple con los parametros");
+})
